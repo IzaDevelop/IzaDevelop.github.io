88eaf23f41276db61c0d28fa12f0f08ba3b367fd
Izabelle Santos <izabelle.sdn@gmail.com>
2022-04-19 15:07:27 -0300

    programação do CEP e Endereço
    uso de Ajax com a função Fetch e da Lib VMasker para máscaras de telefone e CEP

---
+++ simplicity/js/scripts.js
@@ -0,0 +1,45 @@
+const formulario = document.querySelector("form");
+const inputCep = formulario.querySelector("#cep");
+const inputTelefone = formulario.querySelector("#telefone");
+const inputEndereco = formulario.querySelector("#endereco");
+const inputBairro = formulario.querySelector("#bairro");
+const inputCidade = formulario.querySelector("#cidade");
+const inputEstado = formulario.querySelector("#estado");
+const bStatus = formulario.querySelector("#status");
+const botaoLocalizar = formulario.querySelector("#localizar-cep");
+
+botaoLocalizar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    /* acessar o site viacep.com.br */
+
+    /* pegar o cep digitado */
+    let cep = inputCep.value;
+
+    /* cep no padrão da API */
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+    
+    /* ajax: comunicação assíncrona com o viaCEP usando a função chamada fetch */
+    // 1) fazer conexão com a API (ou acessar)
+    fetch(url)
+        // 2) E então, recupere a resposta do acesso no formato JSON
+        .then(resposta => resposta.json())
+            // 3) E então. mostre os dados
+            .then(dados => {
+                if ("erro" in dados) {
+                    bStatus.innerHTML = "CEP não encontrado!";
+                    inputCep.focus();
+                } else {
+                    bStatus.innerHTML = "CEP encontrado!";
+                    inputEndereco.value = dados.logradouro;
+                    inputBairro.value = dados.bairro;
+                    inputCidade.value = dados.localidade;
+                    inputEstado.value = dados.uf;
+                }
+            });
+});
+/* lib vanillaMasker:
+https://github.com/vanilla-masker/vanilla-masker */
+
+VMasker(inputCep).maskPattern("99999-999");
+VMasker(inputTelefone).maskPattern("(99) 9999-9999");
